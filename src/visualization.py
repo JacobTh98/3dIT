cf6f463d6aee7deccfe25f09c9d1d4067cae8505
jt292 <jacob.thoenes@gmail.com>
2023-12-30 11:17:25 +0000

    added images

--- src/visualization.py
+++ src/visualization.py
@@ -197,7 +197,7 @@ def plot_rball(
     ax.view_init(elev=elev, azim=azim)
     plt.tight_layout()
     if save_img:
-        plt.savefig(s_path + "plot_meas_coords_wball.png", dpi=250)
+        plt.savefig(s_path + "plot_meas_coords_wball.png", dpi=250, transparent=True)
     plt.show()
 
 
@@ -267,16 +267,16 @@ def plot_loss_history(history):
     plt.show()
 
 
-def plot_voxel(voxelarray, elev=20, azim=10, save_img=False, s_name="unnamed.pdf"):
+def plot_voxel(voxelarray, elev=20, azim=10, save_img=False, s_name="images/voxels.png"):
     ax = plt.figure(figsize=(6, 6)).add_subplot(projection="3d")
     ax.voxels(voxelarray)
     ax.view_init(azim=azim, elev=elev)
-    ax.set_xlabel("x []")
-    ax.set_ylabel("y []")
-    ax.set_zlabel("z []")
+    ax.set_xlabel("x [voxel]")
+    ax.set_ylabel("y [voxel]")
+    ax.set_zlabel("z [voxel]")
     plt.tight_layout()
     if save_img:
-        plt.savefig(s_name, transparent=True)
+        plt.savefig(s_name,dpi=250, transparent=True)
     plt.show()
 
 
